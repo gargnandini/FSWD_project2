054383001111d749de54fa67f09083257e23e43e
Nandini <nandinigarg1204@gmail.com>
2020-09-24 14:43:39 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 // function App() {
 //   return (
@@ -36,9 +37,11 @@ class App extends Component{
 
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
